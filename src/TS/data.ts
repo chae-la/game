01e4e9ab54b15b0c9bbb41e194091863a59d3e3a
Michaela <michaelachan45@gmail.com>
2024-03-14 11:42:09 +0000

    question bank done

--- src/TS/data.ts
+++ src/TS/data.ts
@@ -2,63 +2,76 @@ import { Question } from "./type";
 
 export const questionBank: Question[] = [
   {
+    questionNum: 1,
     question: "Can a match box?",
     possibleAns: ["Yes", "No", "No, but a tin can", "Yes, one beat Mike Tyson"],
     correctAns: "No, but a tin can",
   },
   {
+    questionNum:2,
     question: ".SDRAWKCAB NOITSEUQ SIHT REQSNA",
     possibleAns: ["KO", "What?", "I don't understand", "Backwards"],
     correctAns: "KO",
   },
   {
+    questionNum:3,
     question: "The answer is REALLY big",
     possibleAns: ["ANSWER", "REALLY Big", "∞", "Whale"],
     correctAns: "Whale",
   },
   {
-    question: "Can a match box?",
-    possibleAns: ["Yes", "No", "No, but a tin can", "Yes, one beat Mike Tyson"],
-    correctAns: "No, but a tin can",
+    questionNum:4,
+    question: "What follows December 2nd?",
+    possibleAns: ["n", "December 3rd", "a Question Mark", "What"],
+    correctAns: "n",
   },
   {
+    questionNum:5,
+    question: "What is the 7th letter of the alphabet",
+    possibleAns: ["a", "e", "g", "h"],
+    correctAns: "h",
+  },
+  {
+    questionNum:6,
+    question: "How many letters in their hand?",
+    image: "./images/letters.png",
+    possibleAns: ["6", "7", "8", "9"],
+    correctAns: "9",
+  },
+  {
+    questionNum: 7,
+    question: "8÷2(2+2)",
+    possibleAns: ["16", "8", "4", "1"],
+    correctAns: "16",
+  },
+  {
+    questionNum: 8,
+    question: "Which of the following do you need to build a green-house?",
+    possibleAns: ["Flowers", "Bricks", "Paint", "Glass"],
+    correctAns: "Paint",
+  },
+  {
+    questionNum: 9,
+    question: "Which is the largest",
+    possibleAns: ["Mars", "Earth", "Galaxy", "Milky Way"],
+    correctAns: "Earth",
+  },
+  {
+    questionNum: 10,
+    question: "How many letters in the answer?",
+    possibleAns: ["One", "Five", "Two", "Four"],
+    correctAns: "Four",
+  },
+  {
+    questionNum: 11,
     question: "What was the answer to question 2?",
     possibleAns: ["This one", "That one", "This?", "Definitely this"],
     correctAns: "No, but a tin can",
   },
   {
-    question: "Can a match box?",
-    possibleAns: ["Yes", "No", "No, but a tin can", "Yes, one beat Mike Tyson"],
-    correctAns: "No, but a tin can",
-  },
-  {
-    question: "Can a match box?",
-    possibleAns: ["Yes", "No", "No, but a tin can", "Yes, one beat Mike Tyson"],
-    correctAns: "No, but a tin can",
-  },
-  {
-    question: "Can a match box?",
-    possibleAns: ["Yes", "No", "No, but a tin can", "Yes, one beat Mike Tyson"],
-    correctAns: "No, but a tin can",
-  },
-  {
-    question: "Can a match box?",
-    possibleAns: ["Yes", "No", "No, but a tin can", "Yes, one beat Mike Tyson"],
-    correctAns: "No, but a tin can",
-  },
-  {
-    question: "Can a match box?",
-    possibleAns: ["Yes", "No", "No, but a tin can", "Yes, one beat Mike Tyson"],
-    correctAns: "No, but a tin can",
-  },
-  {
-    question: "Can a match box?",
-    possibleAns: ["Yes", "No", "No, but a tin can", "Yes, one beat Mike Tyson"],
-    correctAns: "No, but a tin can",
-  },
-  {
-    question: "Can a match box?",
-    possibleAns: ["Yes", "No", "No, but a tin can", "Yes, one beat Mike Tyson"],
-    correctAns: "No, but a tin can",
+    questionNum: 12,
+    question: "What cheese is made backwards?",
+    possibleAns: ["Edam", "Brie", "Mozzarella", "Burrata"],
+    correctAns: "Edam",
   },
 ];
--- src/TS/type.ts
+++ src/TS/type.ts
@@ -1,5 +1,7 @@
 export type Question = {
+    questionNum: number,
     question: string;
+    image ?: string;
     possibleAns: string[];
     correctAns: string;
 }
