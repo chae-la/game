b29f668c18fc6405b57b30ae3512c84209a04abd
Michaela <michaelachan45@gmail.com>
2024-03-14 11:10:05 +0000

    Added styles and ts diles into folders

---
+++ src/TS/data.ts
@@ -0,0 +1,64 @@
+import { Question } from "./type";
+
+export const questionBank: Question[] = [
+  {
+    question: "Can a match box?",
+    possibleAns: ["Yes", "No", "No, but a tin can", "Yes, one beat Mike Tyson"],
+    correctAns: "No, but a tin can",
+  },
+  {
+    question: ".SDRAWKCAB NOITSEUQ SIHT REQSNA",
+    possibleAns: ["KO", "What?", "I don't understand", "Backwards"],
+    correctAns: "KO",
+  },
+  {
+    question: "The answer is REALLY big",
+    possibleAns: ["ANSWER", "REALLY Big", "∞", "Whale"],
+    correctAns: "Whale",
+  },
+  {
+    question: "Can a match box?",
+    possibleAns: ["Yes", "No", "No, but a tin can", "Yes, one beat Mike Tyson"],
+    correctAns: "No, but a tin can",
+  },
+  {
+    question: "What was the answer to question 2?",
+    possibleAns: ["This one", "That one", "This?", "Definitely this"],
+    correctAns: "No, but a tin can",
+  },
+  {
+    question: "Can a match box?",
+    possibleAns: ["Yes", "No", "No, but a tin can", "Yes, one beat Mike Tyson"],
+    correctAns: "No, but a tin can",
+  },
+  {
+    question: "Can a match box?",
+    possibleAns: ["Yes", "No", "No, but a tin can", "Yes, one beat Mike Tyson"],
+    correctAns: "No, but a tin can",
+  },
+  {
+    question: "Can a match box?",
+    possibleAns: ["Yes", "No", "No, but a tin can", "Yes, one beat Mike Tyson"],
+    correctAns: "No, but a tin can",
+  },
+  {
+    question: "Can a match box?",
+    possibleAns: ["Yes", "No", "No, but a tin can", "Yes, one beat Mike Tyson"],
+    correctAns: "No, but a tin can",
+  },
+  {
+    question: "Can a match box?",
+    possibleAns: ["Yes", "No", "No, but a tin can", "Yes, one beat Mike Tyson"],
+    correctAns: "No, but a tin can",
+  },
+  {
+    question: "Can a match box?",
+    possibleAns: ["Yes", "No", "No, but a tin can", "Yes, one beat Mike Tyson"],
+    correctAns: "No, but a tin can",
+  },
+  {
+    question: "Can a match box?",
+    possibleAns: ["Yes", "No", "No, but a tin can", "Yes, one beat Mike Tyson"],
+    correctAns: "No, but a tin can",
+  },
+];
---
+++ src/TS/type.ts
@@ -0,0 +1,5 @@
+export type Question = {
+    question: string;
+    possibleAns: string[];
+    correctAns: string;
+}
